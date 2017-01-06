c7ba14a597f81acb860196f1ebbdcd377d7c7c6b
Sean <haveahopes@gmail.com>
2017-01-06 20:46:29 +0900

    MVC Project Upload

---
+++ mvc/app/src/main/java/com/sean/android/mvc/issue/IssueListActivity.java
@@ -0,0 +1,15 @@
+package com.sean.android.mvc.issue;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sean.android.mvc.R;
+
+public class IssueListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_issue_list);
+    }
+}
