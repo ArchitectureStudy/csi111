31909f2140ecd02afc70d9fa614d638c12068550
Seonil <choi.seonil@joongang.co.kr>
2017-02-09 17:47:19 +0900

    Edit Gradle

---
+++ architecture/mvvm/src/main/java/com/sean/android/mvvmsample/ui/splash/viewmodel/SplashViewModel.java
@@ -0,0 +1,14 @@
+package com.sean.android.mvvmsample.ui.splash.viewmodel;
+
+/**
+ * Created by Seonil on 2017-02-09.
+ */
+
+public interface SplashViewModel {
+
+    String getId();
+
+    String getRepositoryText();
+
+    String getAccessTokenText();
+}
